a26a6ef36bd7e1d6c284ab00e07ffaad12e3c787
Chopas11 <chopasik@gmail.com>
2020-04-22 14:13:45 +0300

    Frontend template release 2.2

---
+++ gulpfile.js
@@ -0,0 +1,98 @@
+let preprocessor = 'sass'; 																	// Preprocessor 
+let fileswatch   = 'html,htm,txt,json,md,woff2'; 											// List of files extensions for watching & hard reload (comma separated)
+let imageswatch  = 'jpg,jpeg,png,webp,svg'; 												// List of images extensions for watching & compression (comma separated)
+
+//---------------------------------------<NodeJS>-------------------------------------------//
+const { src, dest, parallel, series, watch } = require('gulp');								// Gulp
+const sass         = require('gulp-sass');													//  Sass
+const cleancss     = require('gulp-clean-css');												// Clean css
+const concat       = require('gulp-concat');												// Concat files
+const browserSync  = require('browser-sync').create();										// Concat files
+const uglify       = require('gulp-uglify-es').default;										// UglifyJS
+const autoprefixer = require('gulp-autoprefixer');											// -moz- -o- -webkit- etc.
+const imagemin     = require('gulp-imagemin');												// Minification images
+const newer        = require('gulp-newer');													// Minification images too
+const rsync        = require('gulp-rsync');													// Deploy build files
+const del          = require('del');														// del files
+
+function browsersync() {
+	browserSync.init({
+		server: { baseDir: 'build' },
+		notify: false,
+		// online: false, // Work offline without internet connection
+	})
+}
+
+function html() {
+	return src('src/*.html')
+	.pipe(dest('build'))
+}
+
+function styles() {
+	return src('src/' + preprocessor + '/main.*')
+	.pipe(eval(preprocessor)())
+	.pipe(concat('app.min.css'))
+	.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
+	.pipe(dest('build/css'))
+	.pipe(browserSync.stream())
+}
+
+function scripts() {
+	return src([
+		//'node_modules/jquery/dist/jquery.min.js', 										// npm i --save jquery
+		'src/js/app.js'
+		])
+	.pipe(concat('app.min.js'))
+	.pipe(uglify())
+	.pipe(dest('build/js'))
+	.pipe(browserSync.stream())
+}
+
+function images() {
+	return src('src/images/**/*')
+	.pipe(dest('build/images'))
+}
+
+function fonts() {
+	return src('src/fonts/**/*.woff2')
+	.pipe(dest('build/fonts'))
+}
+
+function cleanimg() {
+	return del('build/images/dest/**/*', { force: true })
+}
+
+function deploy() {																		// Rsync
+	return src('build/')
+	.pipe(rsync({
+		root: 'build/',
+		hostname: 'user@server',
+		destination: '/var/www/html',
+		port: 22,
+		// include: ['*.htaccess'], // Included files
+		exclude: ['**/Thumbs.db', '**/*.DS_Store', '**/*.html'], // Excluded files
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+}
+
+function startwatch() {	
+	watch('src/*.html', parallel('html'));
+	watch('src/' + preprocessor + '/**/*', parallel('styles'));
+	watch(['src/**/*.js'], parallel('scripts'));
+	watch(['src/**/*.{' + imageswatch + '}'], parallel('images'));
+	watch(['src/**/*.{' + fileswatch + '}', 'build/*.html']).on('change', browserSync.reload);
+}
+
+exports.browsersync = browsersync;
+exports.html 		= html;
+exports.styles      = styles;
+exports.scripts     = scripts;
+exports.images      = images;
+exports.fonts      	= fonts;
+exports.cleanimg    = cleanimg;
+exports.deploy    	= deploy;
+exports.default     = parallel(html, styles, scripts, images, fonts, browsersync, startwatch);
---
+++ readme.md
@@ -0,0 +1,49 @@
+﻿Gulp frontend template v2.2
+===========================
+
+This template is needed to create front-end of sites using Gulp 4, Sass, Font-awesome, jQuery and Bootstrap.
+The template uses the folder for the source code (/src) and the folder for production files (/build),
+in /build folder files are automatically compiled.
+
+Implemented Rsync plugin, which allows you to quickly deploy finished filesv from /build to the server.
+
+INSTALLATION
+------------
+
+1. You can download repository [from GitHub](https://github.com/) or clone, if you use git an your computer:
+
+      $ git clone https://github.com/
+    
+2. Install NodeJS 12.x on your computer, if you 've not done it before.
+3. Install all npm packages:
+
+      $ npm install
+
+
+4. Customize the template according to the Customize instructions below.
+5. Start template:
+
+      $ gulp
+
+
+6. To deploy finished files on server, follow 5 step of Customize Instruction and run command:
+
+      $ gulp deploy
+
+
+Customize Instruction:
+----------------------
+
+1. Install the desired font in the src/fonts/yourfont-webfont folder in woff2 format and write it in the "Fonts Including" section in the src/sass/_config.sass file (FontAwesome is already installed in the template)
+
+2. Set up the necessary project variables in the app/sass/_config.sass file
+
+3. Install the necessary libraries through npm: 
+      
+        $ npm install i --save-dev library_name
+      
+Tnen, include css library in the "Libraries Including" section in the src/sass/_config.sass file (By default, jQuery and Bootstrap are already in the project, both are turned off), and JS library in the gulpfile.js file
+
+4. You can find the layout of src/index.html, Styles in the file src/sass/main.sass
+
+5. Insert info about server to deploy in function deploy() in file gulpfile.js
